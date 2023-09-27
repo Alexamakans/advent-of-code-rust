47fc08ff93c189ddae1011233c7751b3f2efade8
Alexamakans <fougner.alexamakans@gmail.com>
2023-09-28 00:06:59 +0200

    2015D5P1

--- src/twothousandfifteen/day5/mod.rs
+++ src/twothousandfifteen/day5/mod.rs
@@ -3,7 +3,29 @@ use super::{super::utils::*, YEAR};
 pub struct Solver {}
 impl DaySolver<i32> for Solver {
     fn part_one_driver(&self, input: String) -> i32 {
-        todo!();
+        fn is_nice_string(s: &str) -> bool {
+            let does_not_have_blacklisted_substrings =
+                !(s.contains("ab") || s.contains("cd") || s.contains("pq") || s.contains("xy"));
+            let num_vowels = s
+                .chars()
+                .filter(|c| match c {
+                    'a' | 'e' | 'i' | 'o' | 'u' => true,
+                    _ => false,
+                })
+                .count();
+            let has_double = 'has_double_check: {
+                for i in 1..s.chars().count() {
+                    if s.chars().nth(i).unwrap() == s.chars().nth(i - 1).unwrap() {
+                        break 'has_double_check true;
+                    }
+                }
+                false
+            };
+
+            does_not_have_blacklisted_substrings && num_vowels >= 3 && has_double
+        }
+
+        input.lines().filter(|line| is_nice_string(line)).count() as i32
     }
 
     fn part_two_driver(&self, input: String) -> i32 {
@@ -15,21 +37,31 @@ impl DaySolver<i32> for Solver {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn part_one_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![
+            ("ugknbfddgicrmopn", 1),
+            ("aaa", 1),
+            ("jchzalrnumimnmhp", 0),
+            ("haegwjzuvuyypxyu", 0),
+            ("dvszwmarrgswjxmb", 0),
+        ];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_one_driver(String::from(case.0)), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(
+                solver.part_one_driver(String::from(case.0)),
+                case.1,
+                "input = {}",
+                case.0
+            );
+        }
 
-        // assert_eq!(solver.part_one(), 123);
+        assert_eq!(solver.part_one(), 258);
     }
 
     #[test]
