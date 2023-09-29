12a7d5707a55880c41a9eb456935aaadbfe4806c
Alexamakans <fougner.alexamakans@gmail.com>
2023-09-30 01:39:21 +0200

    2015D10P1 & 2015D10P2

--- src/twothousandfifteen/day10/mod.rs
+++ src/twothousandfifteen/day10/mod.rs
@@ -3,11 +3,23 @@ use super::{super::utils::*, YEAR};
 pub struct Solver {}
 impl DaySolver<i32> for Solver {
     fn part_one_driver(&self, input: String) -> i32 {
-        todo!();
+        let mut sum_length = 0;
+        for line in input.lines() {
+            let encoded = encode(&line, 40);
+            sum_length += encoded.len();
+        }
+
+        sum_length as i32
     }
 
     fn part_two_driver(&self, input: String) -> i32 {
-        todo!();
+        let mut sum_length = 0;
+        for line in input.lines() {
+            let encoded = encode(&line, 50);
+            sum_length += encoded.len();
+        }
+
+        sum_length as i32
     }
 
     fn read_input(&self) -> String {
@@ -15,6 +27,20 @@ impl DaySolver<i32> for Solver {
     }
 }
 
+fn encode(s: &str, iterations: u32) -> String {
+    let mut result = s.to_owned();
+    for _ in 0..iterations {
+        let mut iteration_result = String::new();
+        let sequences = extract_sequences(result.chars());
+        for sequence in sequences.iter() {
+            let length = sequence.0.1 - sequence.0.0;
+            iteration_result.push_str(&format!("{}{}", length, sequence.1));
+        }
+        result = iteration_result;
+    }
+
+    result
+}
 
 #[cfg(test)]
 mod tests {
@@ -22,25 +48,37 @@ mod tests {
 
     #[test]
     fn part_one_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver{};
+        let cases = vec![
+            ("1", 1, "11"),
+            ("11", 1, "21"),
+            ("21", 1, "1211"),
+            ("1211", 1, "111221"),
+            ("111221", 1, "312211"),
+        ];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_one_driver(String::from(case.0)), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(encode(case.0, case.1), case.2, "input = {}", case.0);
+        }
 
-        // assert_eq!(solver.part_one(), 123);
+        assert_eq!(solver.part_one(), 329356);
     }
 
     #[test]
     fn part_two_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver{};
+        let cases = vec![
+            ("1", 1, "11"),
+            ("11", 1, "21"),
+            ("21", 1, "1211"),
+            ("1211", 1, "111221"),
+            ("111221", 1, "312211"),
+        ];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_two_driver(String::from(case.0)), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(encode(case.0, case.1), case.2, "input = {}", case.0);
+        }
 
-        // assert_eq!(solver.part_two(), 123);
+        assert_eq!(solver.part_two(), 4666278);
     }
 }
