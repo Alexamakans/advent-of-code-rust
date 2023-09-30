509a8dd99511c5e11cf0812ce6bd9b494163d495
Alexamakans <fougner.alexamakans@gmail.com>
2023-09-30 16:57:38 +0200

    Actually 2015D13P2

--- src/twothousandfifteen/day13/mod.rs
+++ src/twothousandfifteen/day13/mod.rs
@@ -1,6 +1,6 @@
 // cba researching it but this feels like an optimization problem using doubly linked list with weights on the edges
 
-use std::{cell::RefCell, rc::Rc, collections::HashSet};
+use std::collections::HashSet;
 
 use crate::scanf;
 
@@ -10,7 +10,7 @@ type Interaction = (String, i32, String);
 
 pub struct Solver {}
 impl DaySolver<i32> for Solver {
-    fn part_one_driver(&self, input: String) -> i32 {
+    fn part_one_driver(&self, input: &str) -> i32 {
         let mut interactions = Vec::new();
         let mut people_set = HashSet::new();
         for line in input.lines() {
@@ -20,27 +20,21 @@ impl DaySolver<i32> for Solver {
         }
 
         let people = people_set.into_iter().collect::<Vec<String>>();
-        let mut best_happiness = i32::MIN;
-        for permutation in people.into_permutations() {
-            let first = permutation.first().unwrap();
-            let last = permutation.last().unwrap();
-            let mut happiness = get_happiness(first, last, &interactions);
-
-            let pairs = permutation.iter().take(permutation.len() - 1).zip(permutation.iter().skip(1));
-            for pair in pairs {
-                happiness += get_happiness(pair.0, pair.1, &interactions);
-            }
-
-            if happiness > best_happiness {
-                best_happiness = happiness;
-            }
-        }
-
-        best_happiness
+        get_optimal_happiness(people, &interactions)
     }
 
-    fn part_two_driver(&self, input: String) -> i32 {
-        todo!();
+    fn part_two_driver(&self, input: &str) -> i32 {
+        let mut interactions = Vec::new();
+        let mut people_set = HashSet::new();
+        for line in input.lines() {
+            let interaction = parse_line(line);
+            people_set.insert(interaction.0.clone());
+            interactions.push(interaction);
+        }
+        
+        let mut people = people_set.into_iter().collect::<Vec<String>>();
+        people.push(String::from("Me"));
+        get_optimal_happiness(people, &interactions)
     }
 
     fn read_input(&self) -> String {
@@ -57,11 +51,34 @@ fn parse_line(s: &str) -> Interaction {
     scanf!(modified, ",", String, i32, String)
 }
 
+fn get_optimal_happiness(people: Vec<String>, interactions: &Vec<Interaction>) -> i32 {
+    let mut best_happiness = i32::MIN;
+    for permutation in people.into_permutations() {
+        let first = permutation.first().unwrap();
+        let last = permutation.last().unwrap();
+        let mut happiness = get_happiness(first, last, &interactions);
+
+        let pairs = permutation.iter().take(permutation.len() - 1).zip(permutation.iter().skip(1));
+        for pair in pairs {
+            happiness += get_happiness(pair.0, pair.1, &interactions);
+        }
+
+        if happiness > best_happiness {
+            best_happiness = happiness;
+        }
+    }
+
+    best_happiness
+}
+
 fn get_happiness(
     a: &str,
     b: &str,
     interactions: &Vec<Interaction>,
 ) -> i32 {
+    if a == "Me" || b == "Me" {
+        return 0;
+    }
     let happiness_a = 'found: {
         for interaction in interactions {
             if interaction.0 == a && interaction.2 == b {
@@ -105,7 +122,7 @@ David would gain 41 happiness units by sitting next to Carol.", 330)
         ];
 
         for case in cases {
-            assert_eq!(solver.part_one_driver(String::from(case.0)), case.1, "input = {}", case.0);
+            assert_eq!(solver.part_one_driver(case.0), case.1, "input = {}", case.0);
         }
 
         assert_eq!(solver.part_one(), 618);
@@ -113,13 +130,7 @@ David would gain 41 happiness units by sitting next to Carol.", 330)
 
     #[test]
     fn part_two_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
-
-        // for case in cases {
-        //     assert_eq!(solver.part_two_driver(String::from(case.0)), case.1, "input = {}", case.0);
-        // }
-
-        // assert_eq!(solver.part_two(), 123);
+        let solver = Solver{};
+        assert_eq!(solver.part_two(), 601);
     }
 }
