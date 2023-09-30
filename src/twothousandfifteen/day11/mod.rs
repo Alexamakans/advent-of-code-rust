ebbfa64a9be8b863845d242abbecd841cc5b96cb
Alexamakans <fougner.alexamakans@gmail.com>
2023-09-30 11:37:10 +0200

    2015D11P1 & 2015D11P2

--- src/twothousandfifteen/day11/mod.rs
+++ src/twothousandfifteen/day11/mod.rs
@@ -1,13 +1,13 @@
 use super::{super::utils::*, YEAR};
 
 pub struct Solver {}
-impl DaySolver<i32> for Solver {
-    fn part_one_driver(&self, input: String) -> i32 {
-        todo!();
+impl DaySolver<String> for Solver {
+    fn part_one_driver(&self, input: String) -> String {
+        get_next_password(&input)
     }
 
-    fn part_two_driver(&self, input: String) -> i32 {
-        todo!();
+    fn part_two_driver(&self, input: String) -> String {
+        get_next_password(&get_next_password(&input))
     }
 
     fn read_input(&self) -> String {
@@ -15,6 +15,87 @@ impl DaySolver<i32> for Solver {
     }
 }
 
+fn get_next_password(s: &str) -> String {
+    let values = vec![
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+        "s", "t", "u", "v", "w", "x", "y", "z",
+    ];
+
+    fn get_index_from_value<T: Eq>(values: &Vec<T>, value: T) -> Option<usize> {
+        for (i, e) in values.iter().enumerate() {
+            if *e == value {
+                return Some(i);
+            }
+        }
+        None
+    }
+
+    let mut sequence_generator = values.clone().into_sequence_generator();
+    sequence_generator.set_indices(
+        s.chars()
+            .map(|c| get_index_from_value(&values, &c.to_string()).unwrap())
+            .collect::<Vec<usize>>(),
+    );
+
+    loop {
+        let next = sequence_generator.next().unwrap().join("");
+        if is_valid_password(&next) {
+            return next;
+        }
+    }
+}
+
+fn is_valid_password(s: &str) -> bool {
+    let has_no_blacklisted_characters = !s.contains(&['i', 'o', 'l']);
+    let has_increasing_straight = 'found: {
+        let num_chars = s.chars().count();
+        for i in 0..num_chars - 2 {
+            let chars = vec![
+                s.chars().nth(i).unwrap(),
+                s.chars().nth(i + 1).unwrap(),
+                s.chars().nth(i + 2).unwrap(),
+            ];
+            let mut as_u8 = chars.into_iter().map(|v| v as u8);
+            let (a, b, c) = (
+                as_u8.next().unwrap(),
+                as_u8.next().unwrap(),
+                as_u8.next().unwrap(),
+            );
+            if a == b - 1 && a == c - 2 {
+                break 'found true;
+            }
+        }
+        false
+    };
+    let has_at_least_two_distinct_non_overlapping_pairs = 'found: {
+        let pairs = s
+            .chars()
+            .take(s.len() - 1)
+            .zip(s.chars().skip(1))
+            .collect::<Vec<(char, char)>>();
+        let num_pairs = pairs.len();
+        for i in 0..num_pairs {
+            let a = pairs.get(i).unwrap();
+            if a.0 != a.1 {
+                continue;
+            }
+            for j in i + 2..num_pairs {
+                let b = pairs.get(j).unwrap();
+                if b.0 != b.1 {
+                    continue;
+                }
+                if a != b {
+                    break 'found true;
+                }
+            }
+        }
+        false
+    };
+
+    has_no_blacklisted_characters
+        && has_increasing_straight
+        && has_at_least_two_distinct_non_overlapping_pairs
+}
 
 #[cfg(test)]
 mod tests {
@@ -22,25 +103,24 @@ mod tests {
 
     #[test]
     fn part_one_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![("abcdefgh", "abcdffaa")];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_one_driver(String::from(case.0)), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(
+                solver.part_one_driver(String::from(case.0)),
+                case.1,
+                "input = {}",
+                case.0
+            );
+        }
 
-        // assert_eq!(solver.part_one(), 123);
+        assert_eq!(solver.part_one(), "hxbxxyzz");
     }
 
     #[test]
     fn part_two_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
-
-        // for case in cases {
-        //     assert_eq!(solver.part_two_driver(String::from(case.0)), case.1, "input = {}", case.0);
-        // }
-
-        // assert_eq!(solver.part_two(), 123);
+        let solver = Solver{};
+        assert_eq!(solver.part_two(), "hxcaabcc");
     }
 }
