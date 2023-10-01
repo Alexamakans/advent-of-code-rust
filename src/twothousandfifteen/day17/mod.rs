ac5af5a2658e4fdb1a0b935f83ebee034b1b54aa
Alexamakans <fougner.alexamakans@gmail.com>
2023-10-01 14:17:30 +0200

    2015D17P1 & 2015D17P2

--- src/twothousandfifteen/day17/mod.rs
+++ src/twothousandfifteen/day17/mod.rs
@@ -1,13 +1,25 @@
 use super::{super::utils::*, YEAR};
 
 pub struct Solver {}
-impl DaySolver<i32> for Solver {
-    fn part_one_driver(&self, input: &str) -> i32 {
-        todo!();
+impl DaySolver<u32> for Solver {
+    fn part_one_driver(&self, input: &str) -> u32 {
+        let mut descending_sorted_containers = input
+            .lines()
+            .map(|line| line.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>();
+        descending_sorted_containers.sort_by(|a, b| b.cmp(a));
+        get_combinations_to_reach_target_value(&descending_sorted_containers, 150).len() as u32
     }
 
-    fn part_two_driver(&self, input: &str) -> i32 {
-        todo!();
+    fn part_two_driver(&self, input: &str) -> u32 {
+        let mut descending_sorted_containers = input
+            .lines()
+            .map(|line| line.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>();
+        descending_sorted_containers.sort_by(|a, b| b.cmp(a));
+        let combinations = get_combinations_to_reach_target_value(&descending_sorted_containers, 150);
+        let minimum_possible = combinations.iter().min_by_key(|e| e.len()).unwrap().len();
+        combinations.into_iter().filter(|e| e.len() == minimum_possible).count() as u32
     }
 
     fn read_input(&self) -> String {
@@ -15,32 +27,31 @@ impl DaySolver<i32> for Solver {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn part_one_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![(vec![20, 15, 10, 5, 5], 25, 4)];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_one_driver(case.0), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(
+                get_combinations_to_reach_target_value(&case.0, case.1).len(),
+                case.2,
+                "containers = {:?}, liters = {}",
+                case.0,
+                case.1
+            );
+        }
 
-        // assert_eq!(solver.part_one(), 123);
+        assert_eq!(solver.part_one(), 1304);
     }
 
     #[test]
     fn part_two_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
-
-        // for case in cases {
-        //     assert_eq!(solver.part_two_driver(case.0), case.1, "input = {}", case.0);
-        // }
-
-        // assert_eq!(solver.part_two(), 123);
+        let solver = Solver{};
+        assert_eq!(solver.part_two(), 18);
     }
 }
