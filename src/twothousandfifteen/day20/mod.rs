cce95aaf3df22efa3be4b45a1db058ee3977ccf7
Alexamakans <fougner.alexamakans@gmail.com>
2023-10-05 20:40:55 +0200

    2015D20P1 & 2015D20P2 - fast

--- src/twothousandfifteen/day20/mod.rs
+++ src/twothousandfifteen/day20/mod.rs
@@ -1,25 +1,67 @@
 use super::{super::utils::*, YEAR};
 
 pub struct Solver {}
-impl DaySolver<u32> for Solver {
-    fn part_one_driver(&self, input: &str) -> u32 {
+impl DaySolver<usize> for Solver {
+    fn part_one_driver(&self, input: &str) -> usize {
         let target_presents = input.parse::<u32>().unwrap();
-        let mut house_number = 500000;
-        loop {
-            let presents = sum_of_factors(house_number) * 10;
+        let num_houses = 3_000_000;
+        let presents_per_elf = 10;
+        // 10 + e * 10 because all house numbers are divisible by 1 and itself
+        let mut houses = (0..num_houses).map(|e| presents_per_elf + e * presents_per_elf).collect::<Vec<u32>>();
+
+        for elf_number in 2..num_houses {
+             // first house is account for in our initialization, so we can skip the house it starts at
+            let mut house_number = elf_number * 2;
+            loop {
+                if house_number >= houses.len() as u32 {
+                    break;
+                }
+
+                let presents = houses.get_mut(house_number as usize).unwrap();
+                *presents += elf_number * presents_per_elf;
+                house_number += elf_number;
+            }
+        }
+
+        for (house_number, presents) in houses.into_iter().enumerate() {
             if presents >= target_presents {
                 return house_number;
             }
-            house_number += 1;
-
-            if house_number % 16384 == 0 {
-                println!("{}", house_number);
-            }
         }
+
+        panic!("increase size of houses vector");
     }
 
-    fn part_two_driver(&self, input: &str) -> u32 {
-        todo!();
+    fn part_two_driver(&self, input: &str) -> usize {
+        let target_presents = input.parse::<u32>().unwrap();
+        let num_houses = 3_000_000;
+        let presents_per_elf = 11;
+        // 11 + e * 11 because all house numbers are divisible by 1 and itself
+        let mut houses = (0..num_houses).map(|e| presents_per_elf + e * presents_per_elf).collect::<Vec<u32>>();
+
+        for elf_number in 2..num_houses {
+             // first house is account for in our initialization, so we can skip the house it starts at
+            let mut house_number = elf_number * 2;
+            let mut houses_left_to_visit = 49;
+            while houses_left_to_visit > 0 {
+                if house_number >= houses.len() as u32 {
+                    break;
+                }
+
+                let presents = houses.get_mut(house_number as usize).unwrap();
+                *presents += elf_number * presents_per_elf;
+                house_number += elf_number;
+                houses_left_to_visit -= 1;
+            }
+        }
+
+        for (house_number, presents) in houses.into_iter().enumerate() {
+            if presents >= target_presents {
+                return house_number;
+            }
+        }
+
+        panic!("increase size of houses vector");
     }
 
     fn read_input(&self) -> String {
@@ -33,7 +75,7 @@ mod tests {
 
     #[test]
     fn part_one_works() {
-        let solver = Solver{};
+        let solver = Solver {};
         let cases = vec![("100", 6)];
 
         for case in cases {
@@ -45,13 +87,7 @@ mod tests {
 
     #[test]
     fn part_two_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
-
-        // for case in cases {
-        //     assert_eq!(solver.part_two_driver(case.0), case.1, "input = {}", case.0);
-        // }
-
-        // assert_eq!(solver.part_two(), 123);
+        let solver = Solver{};
+        assert_eq!(solver.part_two(), 705600);
     }
 }
