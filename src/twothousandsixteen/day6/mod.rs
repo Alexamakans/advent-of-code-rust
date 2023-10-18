2f7aca49730b62480059c601b1cb732c61341db1
Alexamakans <fougner.alexander@gmail.com>
2023-10-18 23:13:42 +0200

    2016D6P1 & 2016D6P2

--- src/twothousandsixteen/day6/mod.rs
+++ src/twothousandsixteen/day6/mod.rs
@@ -1,13 +1,59 @@
+use std::collections::HashMap;
+
 use super::{super::utils::*, YEAR};
 
 pub struct Solver {}
-impl DaySolver<i32> for Solver {
-    fn part_one_driver(&self, input: &str) -> i32 {
-        todo!();
+impl DaySolver<String> for Solver {
+    fn part_one_driver(&self, input: &str) -> String {
+        let word_length = input.lines().nth(0).unwrap().len();
+        let mut counters: Vec<HashMap<char, u32>> = Vec::new();
+        for _ in 0..word_length {
+            counters.push(HashMap::new());
+        }
+
+        for line in input.lines() {
+            for (index, c) in line.chars().enumerate() {
+                let m = counters.get_mut(index).unwrap();
+                if !m.contains_key(&c) {
+                    m.insert(c, 0);
+                }
+                let count = m.get_mut(&c).unwrap();
+                *count += 1;
+            }
+        }
+
+        let mut result = String::new();
+        for index in 0..word_length {
+            let c = counters[index].iter().max_by(|a, b| a.1.cmp(b.1)).unwrap();
+            result.push(*c.0);
+        }
+        result
     }
 
-    fn part_two_driver(&self, input: &str) -> i32 {
-        todo!();
+    fn part_two_driver(&self, input: &str) -> String {
+        let word_length = input.lines().nth(0).unwrap().len();
+        let mut counters: Vec<HashMap<char, u32>> = Vec::new();
+        for _ in 0..word_length {
+            counters.push(HashMap::new());
+        }
+
+        for line in input.lines() {
+            for (index, c) in line.chars().enumerate() {
+                let m = counters.get_mut(index).unwrap();
+                if !m.contains_key(&c) {
+                    m.insert(c, 0);
+                }
+                let count = m.get_mut(&c).unwrap();
+                *count += 1;
+            }
+        }
+
+        let mut result = String::new();
+        for index in 0..word_length {
+            let c = counters[index].iter().min_by(|a, b| a.1.cmp(b.1)).unwrap();
+            result.push(*c.0);
+        }
+        result
     }
 
     fn read_input(&self) -> String {
@@ -21,25 +67,61 @@ mod tests {
 
     #[test]
     fn part_one_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![(
+            r"eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar",
+            "easter",
+        )];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_one_driver(case.0), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(solver.part_one_driver(case.0), case.1, "input = {}", case.0);
+        }
 
-        // assert_eq!(solver.part_one(), 123);
+        assert_eq!(solver.part_one(), "wkbvmikb");
     }
 
     #[test]
     fn part_two_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![(
+            r"eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar",
+            "advent",
+        )];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_two_driver(case.0), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(solver.part_two_driver(case.0), case.1, "input = {}", case.0);
+        }
 
-        // assert_eq!(solver.part_two(), 123);
+        assert_eq!(solver.part_two(), "evakwaga");
     }
 }
