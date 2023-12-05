ed7db64bd7c817b3d7263b1073557a7ebad0bd2a
Alexamakans <fougner.alexander@gmail.com>
2023-12-05 07:28:56 +0100

    2023D4P1 & 2023D4P2 & 2023D5P1 & 2023D5P2

--- src/twothousandtwentythree/day4/mod.rs
+++ src/twothousandtwentythree/day4/mod.rs
@@ -1,13 +1,86 @@
+use std::collections::{HashMap, HashSet};
+
 use super::{super::utils::*, YEAR};
 
 pub struct Solver {}
 impl DaySolver<i32> for Solver {
     fn part_one_driver(&self, input: &str) -> i32 {
-        todo!();
+        let mut result = 0;
+        for line in input.lines() {
+            let parts = line.split_once(":");
+            let (card_str, hand_str) = parts.unwrap().1.split_once("|").unwrap();
+            let mut cards = HashSet::new();
+            for c in card_str.split_ascii_whitespace() {
+                cards.insert(c.parse::<i32>().unwrap());
+            }
+
+            let mut hand = HashSet::new();
+            for h in hand_str.split_ascii_whitespace() {
+                hand.insert(h.parse::<i32>().unwrap());
+            }
+
+            let in_both = cards.intersection(&hand);
+            let mut score = 0;
+            for _ in in_both {
+                score *= 2;
+                if score == 0 {
+                    score = 1;
+                }
+            }
+
+            result += score;
+        }
+
+        result
     }
 
     fn part_two_driver(&self, input: &str) -> i32 {
-        todo!();
+        let mut cache = HashMap::new();
+        let mut total_instances = 0;
+        let lines = input.lines().collect::<Vec<&str>>();
+        let mut next_lines = Vec::new();
+        for (index, _) in lines.iter().enumerate() {
+            next_lines.push(index);
+        }
+        while next_lines.len() > 0 {
+            let mut new_next_lines = Vec::new();
+            for line_index in next_lines.clone() {
+                total_instances += 1;
+                if cache.get(&line_index).is_some() {
+                    let c: &Vec<usize> = cache.get(&line_index).unwrap();
+                    for a in c {
+                        new_next_lines.push(*a);
+                    }
+                } else {
+                    let line = lines.get(line_index).expect("should exist");
+                    let parts = line.split_once(":").unwrap();
+                    let (card_str, hand_str) = parts.1.split_once("|").unwrap();
+                    let mut cards = HashSet::new();
+                    for c in card_str.split_ascii_whitespace() {
+                        cards.insert(c.parse::<i32>().unwrap());
+                    }
+
+                    let mut hand = HashSet::new();
+                    for h in hand_str.split_ascii_whitespace() {
+                        hand.insert(h.parse::<i32>().unwrap());
+                    }
+
+                    let in_both = cards.intersection(&hand);
+                    let mut new_lines = Vec::new();
+                    for (index, _) in in_both.enumerate() {
+                        new_lines.push(line_index + index + 1);
+                    }
+
+                    cache.insert(line_index, new_lines.clone());
+                    for l in new_lines {
+                        new_next_lines.push(l);
+                    }
+                }
+            }
+            next_lines = new_next_lines;
+        }
+
+        total_instances as i32
     }
 
     fn read_input(&self) -> String {
@@ -21,25 +94,41 @@ mod tests {
 
     #[test]
     fn part_one_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![(
+            r"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+            13,
+        )];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_one_driver(case.0), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(solver.part_one_driver(case.0), case.1, "input = {}", case.0);
+        }
 
-        // assert_eq!(solver.part_one(), 123);
+        assert_eq!(solver.part_one(), 123);
     }
 
     #[test]
     fn part_two_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![(
+            r"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+            30,
+        )];
 
-        // for case in cases {
-        //     assert_eq!(solver.part_two_driver(case.0), case.1, "input = {}", case.0);
-        // }
+        for case in cases {
+            assert_eq!(solver.part_two_driver(case.0), case.1, "input = {}", case.0);
+        }
 
-        // assert_eq!(solver.part_two(), 123);
+        assert_eq!(solver.part_two(), 123);
     }
 }
--- src/twothousandtwentythree/day5/mod.rs
+++ src/twothousandtwentythree/day5/mod.rs
@@ -1,18 +1,166 @@
+use std::ops::Range;
+
 use super::{super::utils::*, YEAR};
 
 pub struct Solver {}
-impl DaySolver<i32> for Solver {
-    fn part_one_driver(&self, input: &str) -> i32 {
-        todo!();
+impl DaySolver<u64> for Solver {
+    fn part_one_driver(&self, input: &str) -> u64 {
+        solve_part_one(input)
     }
 
-    fn part_two_driver(&self, input: &str) -> i32 {
-        todo!();
+    fn part_two_driver(&self, input: &str) -> u64 {
+        solve_part_two(input)
     }
 
     fn read_input(&self) -> String {
         read_input(YEAR, 5)
     }
+}
+
+fn solve_part_one(s: &str) -> u64 {
+    let processed_input = s.replace("\r", "");
+    let mut sections = processed_input.split("\n\n");
+    let seeds = sections
+        .next()
+        .unwrap()
+        .split_once(": ")
+        .unwrap()
+        .1
+        .split_whitespace()
+        .map(|e| {
+            println!("parsing '{}'", e);
+            e.parse::<u64>().unwrap()
+        })
+        .collect::<Vec<u64>>();
+
+    let seeds_to_soil = parse_map_section(sections.next().unwrap());
+    let soil_to_fertilizer = parse_map_section(sections.next().unwrap());
+    let fertilizer_to_water = parse_map_section(sections.next().unwrap());
+    let water_to_light = parse_map_section(sections.next().unwrap());
+    let light_to_temperature = parse_map_section(sections.next().unwrap());
+    let temperature_to_humidity = parse_map_section(sections.next().unwrap());
+    let humidity_to_location = parse_map_section(sections.next().unwrap());
+
+    let mut best_location = u64::MAX;
+    for seed in seeds {
+        let soil = step(seed, &seeds_to_soil);
+        let fert = step(soil, &soil_to_fertilizer);
+        let water = step(fert, &fertilizer_to_water);
+        let light = step(water, &water_to_light);
+        let temp = step(light, &light_to_temperature);
+        let hum = step(temp, &temperature_to_humidity);
+        let loc = step(hum, &humidity_to_location);
+        println!(
+            "{} -> {} -> {} -> {} -> {} -> {} -> {} -> {}",
+            seed, soil, fert, water, light, temp, hum, loc
+        );
+
+        if loc < best_location {
+            best_location = loc;
+        }
+    }
+
+    best_location
+}
+/**
+ Amazingly, bruteforce worked for my input.
+ Was trying to think of a better solution and it suddenly finished /shrug
+
+ Took about 100 seconds on my machine.
+*/
+fn solve_part_two(s: &str) -> u64 {
+    let processed_input = s.replace("\r", "");
+    let mut sections = processed_input.split("\n\n");
+    let seed_ranges = sections
+        .next()
+        .unwrap()
+        .split_once(": ")
+        .unwrap()
+        .1
+        .split_whitespace()
+        .map(|e| {
+            println!("parsing '{}'", e);
+            e.parse::<u64>().unwrap()
+        })
+        .collect::<Vec<u64>>();
+
+    let mut seeds = Vec::new();
+    for e in seed_ranges.chunks_exact(2) {
+        for seed in e[0]..(e[0] + e[1]) {
+            seeds.push(seed);
+        }
+    }
+
+    let seeds_to_soil = parse_map_section(sections.next().unwrap());
+    let soil_to_fertilizer = parse_map_section(sections.next().unwrap());
+    let fertilizer_to_water = parse_map_section(sections.next().unwrap());
+    let water_to_light = parse_map_section(sections.next().unwrap());
+    let light_to_temperature = parse_map_section(sections.next().unwrap());
+    let temperature_to_humidity = parse_map_section(sections.next().unwrap());
+    let humidity_to_location = parse_map_section(sections.next().unwrap());
+
+    let mut best_location = u64::MAX;
+    for seed in seeds {
+        let soil = step(seed, &seeds_to_soil);
+        let fert = step(soil, &soil_to_fertilizer);
+        let water = step(fert, &fertilizer_to_water);
+        let light = step(water, &water_to_light);
+        let temp = step(light, &light_to_temperature);
+        let hum = step(temp, &temperature_to_humidity);
+        let loc = step(hum, &humidity_to_location);
+
+        if loc < best_location {
+            best_location = loc;
+        }
+    }
+
+    best_location
+}
+fn parse_map_section(section: &str) -> Vec<(Range<u64>, Range<u64>)> {
+    let mut mapping = Vec::new();
+    for line in section.lines().skip(1) {
+        let mut parts = line.split_whitespace();
+        let dst_start = parts.next().unwrap().parse::<u64>().unwrap();
+        let src_start = parts.next().unwrap().parse::<u64>().unwrap();
+        let range_length = parts.next().unwrap().parse::<u64>().unwrap();
+        mapping.push((
+            dst_start..(dst_start + range_length),
+            src_start..(src_start + range_length),
+        ));
+    }
+
+    mapping
+}
+
+fn get(src_num: u64, src: &Range<u64>, dst: &Range<u64>) -> Option<u64> {
+    if src.contains(&src_num) {
+        let index = src_num - src.start;
+        let res = dst.start + index;
+        return Some(res);
+    }
+
+    None
+}
+
+fn step(src_num: u64, mapper: &Vec<(Range<u64>, Range<u64>)>) -> u64 {
+    let mut found = false;
+    let mut num = 0;
+    for m in mapper.iter() {
+        let soil = get(src_num, &m.1, &m.0);
+        match soil {
+            Some(v) => {
+                num = v;
+                found = true;
+                break;
+            }
+            None => continue,
+        }
+    }
+
+    if !found {
+        num = src_num;
+    }
+    num
 }
 
 #[cfg(test)]
@@ -21,25 +169,94 @@ mod tests {
 
     #[test]
     fn part_one_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![(
+            r"seeds: 79 14 55 13
 
-        // for case in cases {
-        //     assert_eq!(solver.part_one_driver(case.0), case.1, "input = {}", case.0);
-        // }
+seed-to-soil map:
+50 98 2
+52 50 48
 
-        // assert_eq!(solver.part_one(), 123);
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4",
+            35,
+        )];
+
+        for case in cases {
+            assert_eq!(solver.part_one_driver(case.0), case.1, "input = {}", case.0);
+        }
+
+        assert_eq!(solver.part_one(), 486613012);
     }
 
     #[test]
     fn part_two_works() {
-        // let solver = Solver{};
-        // let cases = vec![];
+        let solver = Solver {};
+        let cases = vec![(
+            r"seeds: 79 14 55 13
 
-        // for case in cases {
-        //     assert_eq!(solver.part_two_driver(case.0), case.1, "input = {}", case.0);
-        // }
+seed-to-soil map:
+50 98 2
+52 50 48
 
-        // assert_eq!(solver.part_two(), 123);
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4",
+            46,
+        )];
+        for case in cases {
+            assert_eq!(solver.part_two_driver(case.0), case.1, "input = {}", case.0);
+        }
+
+        assert_eq!(solver.part_two(), 123);
     }
 }
